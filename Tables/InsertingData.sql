660b0d41b73f43aaba69165e4f0d2f622acdf704
Clyde Allen T. Yu <124365039+YshikageOne@users.noreply.github.com>
2023-10-16 14:36:16 +0800

    Update InsertingData.sql

--- Tables/InsertingData.sql
+++ Tables/InsertingData.sql
@@ -1,4 +1,4 @@
-INSERT INTO tableName (firstColumn, secondColumn,....); -- in order of the column also skip the primary key (explicit)
+INSERT INTO tableName (firstColumn, secondColumn,....) -- in order of the column also skip the primary key (explicit)
                                                         -- Implicit just skip that part
 
 VALUES ('value1', 'value2',......);
